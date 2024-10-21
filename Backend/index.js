0cf030a908ccd2d251fae069dc6f76aedc0b8a00
Aiman Abid <147660353+Aimanabid@users.noreply.github.com>
2024-10-21 17:00:59 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ const cors = require('cors');
 const app = express()
 // postman se jo data arha usko handle krne keliye
 app.use(express.json())
-app.use(cors()); 
+app.use(cors('https://mern-broadway-pizza.vercel.app/')); 
 
 app.post('/register',async(req,res)=>{
 
